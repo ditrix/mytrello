2df6dff27f1decc1e21b9de869d869f414306ec3
dmvoloshin <ditrix2006@gmail.com>
2024-11-12 19:53:59 +0200

    admin data seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Database\Seeders\App\TestDataSeeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,7 +16,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
+        $admin = \App\Models\User::create([
+            'name' => 'Admin',
+            'email' => 'ditrix2006@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('qwerty123'),
+            'remember_token' => Str::random(10),
+        ]);
+        // $admin->save()
         //$this->call([TestDataSeeder::class]);
 
     }
